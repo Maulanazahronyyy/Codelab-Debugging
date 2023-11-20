3892ca8f5e4262f0fc4bb051eb71dfc40905635f
Maulanazahronyyy <maulanazahronyyy@gmail.com>
2023-11-20 15:30:19 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/Debug/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.Debug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.maza.Debug.R
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Sahrul")
+        names.add("Yoga")
+        names.add("Rony")
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
